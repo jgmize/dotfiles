5a140794807683e4d55dccee71ab6181bb5b8b65
Josh Mize <jgmize@gmail.com>
2021-09-06 01:22:09 -0500

    Switch gitpod image: registry.gitlab.com/jgmize/dotfiles

--- .kaniko-build.sh
+++ .kaniko-build.sh
@@ -2,8 +2,8 @@
 
 : ${dockerfile:=${1:-Dockerfile}}
 : ${contextdir:=${CI_PROJECT_DIR:=.}}
-: ${image:=${CI_REGISTRY_IMAGE:=${2:-jgmize/dotfiles}}}
-: ${tag:=${CI_COMMIT_TAG:=${3:-latest}}}
+: ${image:=${2:-${CI_REGISTRY_IMAGE:=jgmize/dotfiles}}}
+: ${tag:=${3:-${CI_COMMIT_SHORT_SHA:=$(git rev-parse --short HEAD)}}}
 
 mkdir -p /kaniko/.docker
 echo "{\"auths\":{\"$CI_REGISTRY\":{\"auth\":\"$(echo -n ${CI_REGISTRY_USER}:${CI_REGISTRY_PASSWORD} | base64)\"}}}" > /kaniko/.docker/config.json
